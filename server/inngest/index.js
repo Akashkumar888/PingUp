3e8e9986a554d1f75ab0ad089a76de493c6414ce
Akashkumar888 <akashgkr12@gmail.com>
2025-08-17 00:17:39 +0530

    first commit

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,6 @@
 
 import { Inngest } from "inngest";
-import userModel from "../models/user.js";
+import userModel from "../models/User.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
