7cb061517e750d6270827b48a528ef270feba19c
Akashkumar888 <akashgkr12@gmail.com>
2025-08-17 00:27:41 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT=process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
+
 
 
 // Connect DB
